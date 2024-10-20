933b7a2f0f418caa00285e36067329f99cf8e878
CS student <me@CSstudent.local>
2024-10-20 14:08:08 -0700

    commit

--- Project1/command.c
+++ Project1/command.c
@@ -6,218 +6,206 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <fcntl.h>
-#include <sys/syscall.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <libgen.h>
 
+void copyFile(char *src, char *dst);
+void makeDir(const char *name);
 
-void mmkdir(const char *name);
+void displayFile(const char *filename){
 
-void cat(const char *filename){
-	int fd;
-	char buffer[1024];
-	ssize_t bytes_read;
+	// Open File
+	int thisFile = open(filename, O_RDONLY);
 
-	// Open file for reading
-	fd = syscall(SYS_open, filename, O_RDONLY);
-	if (fd == -1){
-		const char *error_msg = "cat error: Could not open file\n";
-		syscall(SYS_write, 1, error_msg, strlen(error_msg));
+	// Check if opened
+	if (thisFile == -1){
+		perror("open() error");
 		return;
 	}
 
-	// Read file content to stdout
-	while ((bytes_read = syscall(SYS_read, fd, buffer, 1024)) > 0){
-		syscall(SYS_write, 1, buffer, bytes_read);
+	// Set contents buffer
+	char contents[4096];
+	ssize_t bytesRead;
+
+	// Fill buffer with contents
+	while ((bytesRead = read(thisFile, contents, sizeof(contents))) > 0){
+		ssize_t bytesWritten = write(1, contents, bytesRead);
+                if (bytesWritten != bytesRead){
+                        perror("write() error");
+                        close(thisFile);
+                        return;
+                }
 	}
 
-	// Close the file after reading
-	syscall(SYS_close, fd);
+	if (bytesRead == -1){
+		perror("read() error");
+	}
+
+	// Close file
+	close(thisFile);
 
 }
 
-void rm(const char *filename){
+void deleteFile(const char *filename){
 	int fd = 1;
 	struct stat file_stat;
 
 	// Check if file exists, or if its directory
 	if (stat(filename, &file_stat) == -1){
 		const char *error_msg = "rm error: File does not exist or cannot be accessed\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 		return;
 	}
 
 	// Check if its a directory
 	if (S_ISDIR(file_stat.st_mode)){
 		const char *error_msg = "rm error: Cannot remove a directory\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 		return;
 	}
 
 	// Use the unlink system call to remove the file
-	if (syscall(SYS_unlink, filename) == -1){
+	if (remove(filename) == -1){
 		const char *error_msg = "rm error: Could not remove file\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 	} else{
 		const char *success_msg = "File removed successfully\n";
-		syscall(SYS_write, fd, success_msg, strlen(success_msg));
+		write(STDOUT_FILENO, success_msg, strlen(success_msg));
 	}
 
 }
 
-void mv(const char *src, const char *dst){
-	int fd = 1;
-	struct stat dst_stat;
-
-	// Extract the directory part
-	char dst_dir[1024];
-	strcpy(dst_dir, dst);
-	char *last_slash = strrchr(dst_dir, '/');
-
-	// If there is a directory
-	if (last_slash != NULL){
-		*last_slash = '\0';
-		if (stat(dst_dir, &dst_stat) == -1){
-			if (errno == ENOENT){
-				mmkdir(dst_dir);
-			} else{
-				const char *error_msg = "mv error: Could not stat destination directory\n";
-				syscall(SYS_write, fd, error_msg, strlen(error_msg));
-				return;
-			}
-		}
-	}
-
-	// Use rename system call to move the file
-	if (syscall(SYS_rename, src, dst) == -1){
-		const char *error_msg = "mv error: Could not move file\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+void moveFile(char *src, char *dst){
+	//Call copyFile and call remove on src file
+	if (src == dst){
+		return;
 	} else{
-		const char *success_msg = "File moved successfully\n";
-		syscall(SYS_write, fd, success_msg, strlen(success_msg));
+		copyFile(src, dst);
+		deleteFile(src);
 	}
-
 }
 
-void cp(const char *src, const char *dst){
-	int src_fd, dst_fd;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
-	int fd = 1;
-	struct stat dst_stat;
-
-	// Extract the directory part
-	char dst_dir[1024];
-	strcpy(dst_dir, dst);
-	char *last_slash = strrchr(dst_dir, '/');
-
-	// If there is a directory
-	if (last_slash != NULL){
-		*last_slash = '\0';
-		if (stat(dst_dir, &dst_stat) == -1){
-			if (errno == ENOENT){
-				mmkdir(dst_dir);
-			} else{
-				const char *error_msg = "cp error: Could not stat destination directory.\n";
-				syscall(SYS_write, fd, error_msg, strlen(error_msg));
-				return;
-			}
-		}
-	}
-
-	// Open src file for reading
-	src_fd = syscall(SYS_open, src, O_RDONLY);
-	if (src_fd == -1){
-		const char *error_msg = "cp error: Could not open source file\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+void copyFile(char *src, char *dst){
+	int sourceFile = open(src, O_RDONLY);
+	if (sourceFile == -1){
+		perror("open() source file error");
 		return;
 	}
 
-	// Open dst file for writing or truncate if already exists
-	dst_fd = syscall(SYS_open, dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (dst_fd == -1){
-		const char *error_msg = "cp error: Could not open destination file\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
-		syscall(SYS_close, src_fd);
-		return;
-	}
+	char *fullDest = malloc(strlen(dst) + strlen(src) + 2);
+	int destinationFile;
+	struct stat destStat;
 
-	// Copy content
-	while ((bytes_read = syscall(SYS_read, src_fd, buffer, 1024)) > 0){
-		bytes_written = syscall(SYS_write, dst_fd, buffer, bytes_read);
-		if (bytes_written != bytes_read){
-			const char *error_msg = "cp error: Error while writing to destination file\n";
-			syscall(SYS_write, fd, error_msg, strlen(error_msg));
-			break;
+	// Check if dst is directory
+	if (lstat(dst, &destStat) == 0 && S_ISDIR(destStat.st_mode)){
+		strcpy(fullDest, dst);
+		strcat(fullDest, "/");
+		strcat(fullDest, basename(src));
+		destinationFile = open(fullDest, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (destinationFile == -1){
+			perror("open() destination file error");
+			close(sourceFile);
+			return;
+		}
+	} else{
+		destinationFile = open(dst, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (destinationFile == -1){
+			perror("open() destination file error");
+                        close(sourceFile);
+                        return;
 		}
 	}
 
-	// Close both files
-	syscall(SYS_close, src_fd);
-	syscall(SYS_close, dst_fd);
+	// Copy contents from src to dst
+	char contents[4096];
+	ssize_t bytesRead;
+	while ((bytesRead = read(sourceFile, contents, sizeof(contents))) > 0){
+		ssize_t bytesWritten = write(destinationFile, contents, bytesRead);
+		if (bytesWritten != bytesRead){
+			perror("write() error");
+                        close(sourceFile);
+			close(destinationFile);
+                        return;
+		}
+	}
 
-	const char *success_msg = "File copied succesfully\n";
-	syscall(SYS_write, fd, success_msg, strlen(success_msg));
+	if (bytesRead == -1){
+		perror("read() error");
+	}
 
+	//Close all files
+	if (close(sourceFile) == -1){
+		perror("close() source file error");
+	}
+
+	if (close(destinationFile) == -1){
+                perror("read() destination file error");
+        }
+
+	free(fullDest);
 }
 
-void cd(const char *path){
-	int ret = syscall(SYS_chdir, path);
+void changeDir(const char *path){
+	int ret = chdir(path);
 	int fd = 1;
 
 	if (ret == -1){
 		const char *error_msg = "cd error: Could not change directory\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 	} else{
 		const char *success_msg = "Directory changed successfully\n";
-		syscall(SYS_write, fd, success_msg, strlen(success_msg));
+		write(STDOUT_FILENO, success_msg, strlen(success_msg));
 	}
 }
 
-void mmkdir(const char *name){
-	int ret = syscall(SYS_mkdir, name, 0777);
+void makeDir(const char *name){
+	int ret = mkdir(name, 0777);
 	int fd = 1;
 
 	if (ret == -1){
 		const char *error_msg = "mkdir error: Could not create directory\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 	} else{
 		const char *success_msg = "Directory created successfully\n";
-		syscall(SYS_write, fd, success_msg, strlen(success_msg));
+		write(STDOUT_FILENO, success_msg, strlen(success_msg));
 	}
 }
 
-void pwd(){
+void showCurrentDir(){
 	char cwd[PATH_MAX];
 	ssize_t ret;
 	int fd = 1;
 
 	if (getcwd(cwd, sizeof(cwd)) != NULL){
-		ret = syscall(SYS_write, fd, cwd, strlen(cwd));
-		syscall(SYS_write, fd, "\n", 1);
+		write(STDOUT_FILENO, cwd, strlen(cwd));
+		write(STDOUT_FILENO, "\n", strlen("\n"));
 	} else{
 		const char *error_msg = "pwd error\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+		write(STDOUT_FILENO, error_msg, strlen(error_msg));
 	}
 }
 
-void ls(){
+void listDir(){
+	DIR *cur_dir;
 	struct dirent *entry;
-	DIR *dp = opendir(".");
-	int fd = 1;
 
-	if (dp == NULL){
-		const char *error_msg = "Failed to open directory\n";
-		syscall(SYS_write, fd, error_msg, strlen(error_msg));
+	// Open current directory
+	cur_dir = opendir(".");
+	if (cur_dir == NULL){
+		perror("opendir");
 		return;
 	}
 
-	while ((entry = readdir(dp)) != NULL){
-		if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
-			syscall(SYS_write, fd, entry->d_name, strlen(entry->d_name));
-			syscall(SYS_write, fd, "\n", 1);
-		}
+	// Read and write
+	while ((entry = readdir(cur_dir)) != NULL){
+		write(1, entry->d_name, strlen(entry->d_name));
+		write(1, " ", 1);
 	}
 
-	closedir(dp);
+	//Write new line
+	write(1, "\n", 1);
+
+	// Close directory
+	closedir(cur_dir);
 }
--- Project1/main.c
+++ Project1/main.c
@@ -8,103 +8,126 @@
 
 #define _GNU_SOURCE
 
-char *trim(char *str){
-	char *end;
+void interactive_mode(){
 
-	// Trim leading space
-	while(isspace((unsigned char)*str)) str++;
+	//declear line_buffer
+        size_t len = 128;
+        char* line_buf = malloc (len);
 
-	// If all spaces, return empty str
-	if (*str == 0){
-		return str;
-	}
+	command_line large_token_buffer;
+        command_line small_token_buffer;
 
-	// Trim trailing space
-	end = str + strlen(str) - 1;
-	while(end > str && isspace((unsigned char)*end)) end--;
 
-	// Write new null terminator
-	*(end + 1) = '\0';
+        while(1){
+                printf(">>> ");
 
-	return str;
-}
+                getline(&line_buf, &len, stdin);
 
-void execute_command(const char *cmd){
-	char *command = strdup(cmd);
-	char *token = strtok(command, " ");
+		large_token_buffer = str_filler (line_buf, ";");
 
-	//printf("%s\n", token);
-	//return;
-	if (strcmp(token, "exit") == 0){
-		free(command);
-		exit(0);
+		//iterate through each large token
+                for (int i = 0; large_token_buffer.command_list[i] != NULL; i++){
+                        //smaller token is seperated by " "(space bar)
+                        small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
 
-	} else if(strcmp(token, "ls") == 0){
-		ls();
+                        //iterate through each smaller token to print
+			int numToke = 0;
+                        for (int j = 0; small_token_buffer.command_list[j] != NULL; j++){
+                                numToke += 1;
+                        }
 
-	} else if(strcmp(token, "pwd") == 0){
-		pwd();
+			if (small_token_buffer.command_list[0] == NULL){
+				free_command_line(&small_token_buffer);
+				break;
+			}
 
-	} else if(strcmp(token, "mkdir") == 0){
-		token = strtok(NULL, " ");
-		if (token != NULL){
-			mmkdir(token);
-		} else{
-			fprintf(stderr, "mkdir error: missing directory name\n");
-		}
-	} else if(strcmp(token, "cd") == 0){
-		token = strtok(NULL, " ");
-                if (token != NULL){
-                        cd(token);
-                } else{
-                        fprintf(stderr, "cd error: missing path\n");
+			// If chain to find command with small_token_buffer.command_list[0]
+			if (strcmp(small_token_buffer.command_list[0], "exit") == 0){
+                       		free_command_line(&small_token_buffer);
+                        	memset (&small_token_buffer, 0, 0);
+                		free_command_line(&large_token_buffer);
+                		memset (&large_token_buffer, 0, 0);
+				free(line_buf);
+				exit(0);
+			} else if(strcmp(small_token_buffer.command_list[0], "ls") == 0){
+				if (numToke == 1){
+					listDir();
+				} else{
+					printf ("Error: Unsupported parameters for command: ls\n");
+				}
+			} else if(strcmp(small_token_buffer.command_list[0], "pwd") == 0){
+				if (numToke == 1){
+					showCurrentDir();
+				} else{
+					printf ("Error: Unsupported parameters for command: pwd\n");
+				}
+			} else if(strcmp(small_token_buffer.command_list[0], "mkdir") == 0){
+				if (numToke == 2){
+					makeDir(small_token_buffer.command_list[1]);
+				} else{
+					printf ("Error: Unsupported parameters for command: mkdir\n");
+				}
+			} else if(strcmp(small_token_buffer.command_list[0], "cd") == 0){
+                                if (numToke == 2){
+                                        changeDir(small_token_buffer.command_list[1]);
+                                } else{
+                                        printf ("Error: Unsupported parameters for command: cd\n");
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "cp") == 0){
+                                if (numToke == 3){
+                                        copyFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+                                } else{
+                                        printf ("Error: Unsupported parameters for command: cp\n");
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "mv") == 0){
+                                if (numToke == 3){
+                                        moveFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+                                } else{
+                                        printf ("Error: Unsupported parameters for command: mv\n");
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "rm") == 0){
+                                if (numToke == 2){
+                                        deleteFile(small_token_buffer.command_list[1]);
+                                } else{
+                                        printf ("Error: Unsupported parameters for command: rm\n");
+                                }
+			} else if(strcmp(small_token_buffer.command_list[0], "cat") == 0){
+                                if (numToke == 2){
+                                        displayFile(small_token_buffer.command_list[1]);
+                                } else{
+                                        printf ("Error: Unsupported parameters for command: rm\n");
+                                }
+                        } else{
+				printf ("Error: Unrecognized command: %s\n", small_token_buffer.command_list[0]);
+			}
+			//free smaller tokens and reset variable
+                        free_command_line(&small_token_buffer);
+                        memset (&small_token_buffer, 0, 0);
                 }
-	} else if(strcmp(token, "rm") == 0){
-		token = strtok(NULL, " ");
-                if (token != NULL){
-                        rm(token);
-                } else{
-                        fprintf(stderr, "rm error: missing filename\n");
-                }
-	} else if(strcmp(token, "cat") == 0){
-		token = strtok(NULL, " ");
-                if (token != NULL){
-                        cat(token);
-                } else{
-                        fprintf(stderr, "cat error: missing file name\n");
-                }
-	} else if(strcmp(token, "mv") == 0){
-		char *src = strtok(NULL, " ");
-		char *dst = strtok(NULL, " ");
-                if (src != NULL && dst != NULL){
-                        mv(src, dst);
-                } else{
-                        fprintf(stderr, "mv error: missing arguments\n");
-                }
-	} else if(strcmp(token, "cp") == 0){
-                char *src = strtok(NULL, " ");
-                char *dst = strtok(NULL, " ");
-                if (src != NULL && dst != NULL){
-                        cp(src, dst);
-                } else{
-                        fprintf(stderr, "cp error: missing arguments\n");
-                }
-	} else{
-		fprintf(stderr, "Unknown command: %s\n", token);
-	}
+		//free large token and reset variable
+               	free_command_line(&large_token_buffer);
+              	memset (&large_token_buffer, 0, 0);
+        }
 
-	free(command);
+        free(line_buf);
 }
 
 void file_mode(char *filename){
-
 	//opening file to read
 	FILE *inFPtr;
 	inFPtr = fopen (filename, "r");
 
+	if (inFPtr == NULL){
+		const char *error_msg = "Error: Invalid file\n";
+                write(STDOUT_FILENO, error_msg, strlen(error_msg));
+		exit(0);
+	}
+
 	//declear line_buffer
 	size_t len = 128;
 	char* line_buf = malloc (len);
+
+	freopen("output.txt", "w+", stdout);
 
 	command_line large_token_buffer;
 	command_line small_token_buffer;
@@ -112,26 +135,98 @@ void file_mode(char *filename){
 	int line_num = 0;
 
 	//loop until the file is over
-	while (getline (&line_buf, &len, inFPtr) != -1)
-	{
-		printf ("Line %d:\n", ++line_num);
-
-		//tokenize line buffer
-		//large token is seperated by ";"
+	while (getline (&line_buf, &len, inFPtr) != -1){
+		//tokenize line buffer, large token is seperated by ";"
 		large_token_buffer = str_filler (line_buf, ";");
-		//iterate through each large token
-		for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
-		{
-			printf ("\tLine segment %d:\n", i + 1);
 
-			//tokenize large buffer
-			//smaller token is seperated by " "(space bar)
+		//iterate through each large token
+		for (int i = 0; large_token_buffer.command_list[i] != NULL; i++){
+			//tokenize large buffer, smaller token is seperated by " "(space bar)
 			small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
 
 			//iterate through each smaller token to print
-			for (int j = 0; small_token_buffer.command_list[j] != NULL; j++)
-			{
-				printf ("\t\tToken %d: %s\n", j + 1, small_token_buffer.command_list[j]);
+                        int numToke = 0;
+                        for (int j = 0; small_token_buffer.command_list[j] != NULL; j++){
+                                numToke += 1;
+                        }
+
+                        if (small_token_buffer.command_list[0] == NULL){
+				free_command_line(&small_token_buffer);
+                                break;
+                        }
+
+                        // If chain to find command with small_token_buffer.command_list[0]
+                        if (strcmp(small_token_buffer.command_list[0], "exit") == 0){
+                                free_command_line(&small_token_buffer);
+                                memset (&small_token_buffer, 0, 0);
+				free_command_line (&large_token_buffer);
+                		memset (&large_token_buffer, 0, 0);
+                                free(line_buf);
+				fclose(inFPtr);
+                                exit(0);
+                        } else if(strcmp(small_token_buffer.command_list[0], "ls") == 0){
+                                if (numToke == 1){
+                                        listDir();
+                                } else{
+					const char *error_msg = "Error: Unsupported parameters for command: ls\n";
+                                        write(STDOUT_FILENO, error_msg, strlen(error_msg));
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "pwd") == 0){
+                                if (numToke == 1){
+                                        showCurrentDir();
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: pwd\n";
+					write(STDOUT_FILENO, error_msg, strlen(error_msg));
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "mkdir") == 0){
+                                if (numToke == 2){
+                                        makeDir(small_token_buffer.command_list[1]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: mkdir\n";
+					write(STDOUT_FILENO, error_msg, strlen(error_msg));
+                                }
+                        } else if(strcmp(small_token_buffer.command_list[0], "cd") == 0){
+                                if (numToke == 2){
+                                        changeDir(small_token_buffer.command_list[1]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: cd\n";
+                                	write(STDOUT_FILENO, error_msg, strlen(error_msg));
+				}
+			} else if(strcmp(small_token_buffer.command_list[0], "cp") == 0){
+                                if (numToke == 3){
+                                        copyFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: cp\n";
+                                	write(STDOUT_FILENO, error_msg, strlen(error_msg));
+				}
+                        } else if(strcmp(small_token_buffer.command_list[0], "mv") == 0){
+                                if (numToke == 3){
+                                        moveFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: mv\n";
+                                	write(STDOUT_FILENO, error_msg, strlen(error_msg));
+				}
+                        } else if(strcmp(small_token_buffer.command_list[0], "rm") == 0){
+                                if (numToke == 2){
+                                        deleteFile(small_token_buffer.command_list[1]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: rm\n";
+                                	write(STDOUT_FILENO, error_msg, strlen(error_msg));
+				}
+                        } else if(strcmp(small_token_buffer.command_list[0], "cat") == 0){
+                                if (numToke == 2){
+                                        displayFile(small_token_buffer.command_list[1]);
+                                } else{
+                                        const char *error_msg = "Error: Unsupported parameters for command: rm\n";
+                                	write(STDOUT_FILENO, error_msg, strlen(error_msg));
+				}
+			}  else{
+                           	const char *error_msg = "Error: Unrecognized command: ";
+				size_t total_len = strlen(error_msg) + strlen(small_token_buffer.command_list[0]) + 2;
+				char *full_error_msg = (char *)malloc(total_len);
+				sprintf(full_error_msg, "%s%s\n", error_msg, small_token_buffer.command_list[0]);
+                        	write(STDOUT_FILENO, full_error_msg, strlen(full_error_msg));
+				free(full_error_msg);
 			}
 
 			//free smaller tokens and reset variable
@@ -139,71 +234,28 @@ void file_mode(char *filename){
 			memset (&small_token_buffer, 0, 0);
 		}
 
-		//free smaller tokens and reset variable
+		//free large token and reset variable
 		free_command_line (&large_token_buffer);
 		memset (&large_token_buffer, 0, 0);
 	}
+
+	// Close and free buffer
 	fclose(inFPtr);
-	//free line buffer
 	free (line_buf);
-}
 
-void interactive_mode(){
-	//ls();
-	//pwd();
-	//mmkdir("testDIR");
-	//cd("..");
-	//cp("README.txt", "test/test.txt");
-	//mv("README.txt", "test.txt");
-	//rm("test/test.txt");
-	//cat("test/test.txt");
-
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	while(1){
-		printf(">>> ");
-
-		read = getline(&line, &len, stdin);
-		if (read == -1){
-			free(line);
-			break;
-		}
-
-		// Remove trailing newline character
-		if (line[read - 1] == '\n'){
-			line[read - 1] = '\0';
-		}
-
-		// Split commands by semicolon (;)
-		char *cmd = strtok(line, ";");
-		while (cmd != NULL){
-			cmd = trim(cmd);
-
-			if (strlen(cmd) > 0){
-				//printf("%s\n", cmd);
-				execute_command(cmd);
-			}
-
-			cmd = strtok(NULL, ";");
-		}
-	}
-
-	free(line);
 }
 
 int main(int argc, char* argv[]){
-        // Check for file mode, or interactive mode
+	// Check for file mode, or interactive mode
         if (argc == 3 && strcmp(argv[1], "-f") == 0){
-              file_mode(argv[2]);
+		file_mode(argv[2]);
         }else if (argc == 1){
-              interactive_mode();
+		interactive_mode();
         }else{
               fprintf(stderr, "Usage: %s [-f filename\n", argv[0]);
               return EXIT_FAILURE;
         }
 
-        return EXIT_FAILURE;
+	return EXIT_FAILURE;
 }
 
