505123b390ff0909167d61c53af1506d8790d9dc
arturod <6arturodiaz@gmail.com>
2024-11-15 11:30:22 -0800

    Added project 3

--- Lab7/pipes.c
+++ Lab7/pipes.c
@@ -21,42 +21,47 @@ void* simulate_work(void* arg);
 
 int main(int argc, char* argv[]){
 
+	// create our array of threads :)
+	thread_ids = (pthread_t *)malloc(sizeof(pthread_t) * NUM_WORKERS);
 
-	thread_ids = (pthread_t *)malloc(sizeof(pthread_t) * NUM_WORKERS);		// create our array of threads :)
+	// initialize the locks
+	pthread_mutex_init(&counter_lock, NULL);
+	pthread_mutex_init(&pipe_lock, NULL);
 
-	pthread_mutex_init(&counter_lock, NULL);		// initialize the lock
-	pthread_mutex_init(&pipe_lock, NULL);		// initialize the lock
-
-	if (pipe(pipe_fd) == -1) {			// init the pipe!
+	// init the pipe!
+	if (pipe(pipe_fd) == -1) {
         perror("pipe failed");
         exit(EXIT_FAILURE);
     }
 
+    // give some fake "ids"!
 	int numbers[NUM_WORKERS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-
+	// create thread, give job, and cast args to void pointer!
 	for(int i = 0; i < NUM_WORKERS; i++){
-
-		pthread_create(&thread_ids[i], NULL, simulate_work, (void*)&(numbers[i]));		// create thread, give job, and cast args to void pointer!
-
+		pthread_create(&thread_ids[i], NULL, simulate_work, (void*)&(numbers[i]));
 	}
 
+	// wait on our threads to rejoin main thread
 	for (int j = 0; j < NUM_WORKERS; ++j){
-
-		pthread_join(thread_ids[j], NULL);			// wait on our threads to rejoin main thread
-
+		pthread_join(thread_ids[j], NULL);
 	}
 
-	close(pipe_fd[1]);		// close write end of pipe
+	// close write end of pipe
+	close(pipe_fd[1]);
 
+	// Read from the pipe
 	char buffer[100];
     ssize_t bytes_read;
     while ((bytes_read = read(pipe_fd[0], buffer, sizeof(buffer))) > 0)
         fwrite(buffer, 1, bytes_read, stdout);
 
+    // lock the critical section to prevent race condition!
 	pthread_mutex_destroy(&counter_lock);
 	pthread_mutex_destroy(&pipe_lock);
-	close(pipe_fd[0]);			// close read end
+
+	// close read end
+	close(pipe_fd[0]);
     free(thread_ids);
 
 
@@ -66,7 +71,8 @@ int main(int argc, char* argv[]){
 
 void* simulate_work(void* arg){
 
-	int *numbers = (int *)arg;		// cast back to the type we are expecting!
+	// cast back to the type we are expecting!
+	int *numbers = (int *)arg;
 
 	
 	for (int j = 0; j < 100; j++){
@@ -76,10 +82,13 @@ void* simulate_work(void* arg){
 		pthread_mutex_unlock(&counter_lock);	
 	}
 	
-	pthread_mutex_lock(&pipe_lock);	// lock the critical section to prevent race condition!
+	// lock the critical section to prevent race condition!
+	pthread_mutex_lock(&pipe_lock);
 	char message[100];
     snprintf(message, sizeof(message), "Thread %d finished work, counter = %d\n", *numbers, counter);
-    write(pipe_fd[1], message, strlen(message)); 	// write to the pipe	
+
+    // write to the pipe
+    write(pipe_fd[1], message, strlen(message));	
     pthread_mutex_unlock(&pipe_lock);	
 
 	pthread_exit(NULL);
---
+++ Project3/part1/bank.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+#include <string.h>
+#include "account.h"
+#include "string_parser.h"
+
+int main(int argc, char *argv[]) {
+	// Check for file mode, or interactive mode
+    if (argc == 2){
+		//file_mode(argv[1]);
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+    }else{
+          fprintf(stderr, "Usage: %s filename\n", argv[0]);
+          return EXIT_FAILURE;
+    }
+
+	return EXIT_FAILURE;
+}
